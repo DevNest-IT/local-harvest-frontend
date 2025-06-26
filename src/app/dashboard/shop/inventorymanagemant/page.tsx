6415c6565f5e653b5eb02fb97c6b5d4ac2e27039
Shahir <amijim420@gmail.com>
2025-06-26 08:06:16 +0600

    #changelog3 (For Deployment)

--- src/app/dashboard/shop/inventorymanagemant/page.tsx
+++ src/app/dashboard/shop/inventorymanagemant/page.tsx
@@ -1,9 +1,8 @@
+'use client';
 import InventoryTable from "@/app/dashboard/shop/inventorymanagemant/inventory-management";
 
-export default function App(){
-
-
-    return(
-        <InventoryTable/>
+export default function App() {
+    return (
+        <InventoryTable onInventoryUpdate={() => {}} />
     );
 }
